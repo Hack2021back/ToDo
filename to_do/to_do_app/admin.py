39ddfe66ce2a96666510953add1717ce72b32f6a
Illia Vodolieiev TI-01 <illuhavoda2003@gmail.com>
2021-12-04 11:24:34 +0200

    check

--- to_do/to_do_app/admin.py
+++ to_do/to_do_app/admin.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
 from .models import *
 
+class PostForm(forms.ModelForm):
+    def clean(self):
+        start_data = self.cleaned_data['start_data']
+        end_data = self.cleaned_data['end_data']
+        if start_data > end_data:
+            raise forms.ValidationError({'start_data': "Cant be more end date!"})
+
 class ToDoListAdmin(admin.ModelAdmin):
+    form = PostForm
     list_display = ('task', 'start_data', 'end_data', 'priority')
     search_fields = ('task', 'start_data')
     list_filter = ('start_data', 'end_data', 'priority')
 
+
 admin.site.register(ToDoList, ToDoListAdmin)
+
--- to_do/to_do_app/models.py
+++ to_do/to_do_app/models.py
@@ -1,4 +1,13 @@
+import http
+
 from django.db import models
+from datetime import date
+
+from django.core.validators import MaxValueValidator
+from django.db import models
+from rest_framework.exceptions import ValidationError
+from django import forms
+
 
 class ToDoList(models.Model):
     CHOICES = (
@@ -9,7 +18,33 @@ class ToDoList(models.Model):
     )
     task = models.CharField(max_length=100)
     description = models.TextField(blank=True)
-    start_data = models.DateField()
     end_data = models.DateField(blank=True)
+    start_data = models.DateField()
     time = models.TimeField()
     priority = models.CharField(max_length=10, choices=CHOICES)
+
+    class Meta:
+        verbose_name = "Plans"
+        verbose_name_plural = "Plans"
+        ordering = ['priority', 'task']
+
+    def save(self, *args, **kwargs):
+        if self.start_data > self.end_data:
+            raise forms.ValidationError("ABOBA")
+        super(ToDoList, self).save(args, **kwargs)
+
+# class MemberRegistration(models.Model):
+#     purchase_date=models.DateField(
+#     helptext=('Enter the date of purchase'),
+#         validators=[MaxValueValidator(limit_value=date.today)],
+#         verbosename=('purchase date')
+#     )
+#
+#     class Meta:
+#         constraints = [
+#             models.CheckConstraint(
+#                 check=models.Q(purchase_date__lte=Now()),
+#                 name='purchase_date_cannot_be_future_dated'
+#             )
+#         ]
+
